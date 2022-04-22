d2c14a1ebfd80c13003baa87a1e0d36b32f65fb3
Caden Turpin <c.turpin@admin.attica.k12.in.us>
2022-04-22 10:12:51 -0400

    Changed Text on start

--- StartScene.js
+++ StartScene.js
@@ -12,7 +12,7 @@ class StartScene extends Phaser.Scene {
   const titleScreen = this.physics.add.staticGroup();
   titleScreen.create(225, 90, 'title').setScale(.5, .5).refreshBody();
 
-		this.add.text( 140, 175, 'Click to start!', {fill: '#FAB01B', fontSize: '20px'})
+		this.add.text( 140, 175, 'Click to start LOSER!', {fill: '#FAB01B', fontSize: '20px'})
   		this.add.text( 20, 200, 'Use left and right arrows to dodge!', {fill: '#FAB01B', fontSize: '20px'})
 	    this.add.text( 90, 260, 'Dodge to gain points!', {fill: '#FAB01B', fontSize: '20px'})
   		this.add.text( 10, 300, 'Press down arrow to pull up a protective barrier!', {fill: '#FAB01B', fontSize: '15px'})
